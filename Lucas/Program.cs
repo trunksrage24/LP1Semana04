351bde6e1b1ca98a367b924522fba951119cf8d2
Vitor <vitor.daniel@outlook.pt>
2023-03-23 17:08:10 +0000

    created lucas sequence method

---
+++ Lucas/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Lucas
+{
+    class Program
+    {
+        private static int Lucas(int n)
+        {
+            if (n==0)
+                return 2;
+
+            else if (n==1)
+                return 1;
+
+            else
+                return (Lucas(n - 1) + Lucas(n - 2));
+        }
+
+        static void Main(string[] args)
+        {
+            int UserInput = int.Parse(args[0]);
+            int Input = Lucas(UserInput);
+
+            Console.WriteLine(Input);
+            Console.WriteLine("hello");
+        }
+    }
+}
