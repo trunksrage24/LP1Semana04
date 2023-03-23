111adfa75fdf02d8554b95fdbbd0e7034c3a5f70
Vitor <vitor.daniel@outlook.pt>
2023-03-23 15:50:33 +0000

    finished ex 1

--- Power2Method/Program.cs
+++ Power2Method/Program.cs
@@ -6,7 +6,7 @@ namespace Power2Method
     {
         private static void PowersOf2Until5()
         {
-            for(int i = 1; i <= (1 << 5); i << 1)
+            for(int i = 1; i <= (1 << 5); i = i << 1)
             {
             Console.WriteLine(i);
             }
@@ -14,7 +14,8 @@ namespace Power2Method
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            PowersOf2Until5();
+            PowersOf2Until5();
         }
     }
 }
