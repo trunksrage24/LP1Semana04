0263e639edc56a08eaf6b7941ebbe42c9bc013b6
Vitor <vitor.daniel@outlook.pt>
2023-03-23 16:01:18 +0000

    created new method PowersOf2UntilN()

---
+++ FamousQuotes/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace FamousQuotes
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("You talking to me?");
+            MyPrecious();
+            Console.WriteLine("Dodge this");
+            Hasta();
+        }
+
+        private static void Hasta()
+        {
+            TheMoney();
+            Console.WriteLine("Hasta la vista, baby.");
+            MyPrecious();
+        }
+
+        private static void TheMoney()
+        {
+            Console.WriteLine("Show me the money!");
+        }
+
+        private static void MyPrecious()
+        {
+            TheMoney();
+            Console.WriteLine("My precious.");
+        }
+    }
+}
--- Power2Method/Program.cs
+++ Power2Method/Program.cs
@@ -4,6 +4,7 @@ namespace Power2Method
 {
     class Program
     {
+        //method for shift 1 five times
         private static void PowersOf2Until5()
         {
             for(int i = 1; i <= (1 << 5); i = i << 1)
@@ -12,10 +13,21 @@ namespace Power2Method
             }
         }
 
+        private static void PowersOf2UntilN(int n)
+        {
+            for (int i = 1; i <= ( 1 << n ); i = i << 1)
+            {  
+                Console.WriteLine(i);
+            }
+        }
+
+        //main method
         static void Main(string[] args)
         {
             PowersOf2Until5();
             PowersOf2Until5();
+            PowersOf2UntilN(4);
+            PowersOf2UntilN(6);
         }
     }
 }
