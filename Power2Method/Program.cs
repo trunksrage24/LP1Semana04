25ae2bbd9561d169bc665f41ed606f2d73b02be0
Vitor <vitor.daniel@outlook.pt>
2023-03-23 16:21:18 +0000

    added coments and documentation for ex 6

--- Power2Method/Program.cs
+++ Power2Method/Program.cs
@@ -27,6 +27,12 @@ namespace Power2Method
             }
         }
 
+        //overloading method with 2 param
+        /// <summary>
+        /// overloading method with 2 param
+        /// </summary>
+        /// <param name="n1">shift from here</param>
+        /// <param name="n2">shift till here</param>
         private static void PowersOf2UntilN(int n1, int n2)
         {
             for (int i = (1 << n1); i <= (1 << n2); i = i << 1)
@@ -42,12 +48,15 @@ namespace Power2Method
         /// <param name="args">method accepts strings as type</param>
         static void Main(string[] args)
         {
+            //call method twice
             PowersOf2Until5();
             PowersOf2Until5();
 
+            //call method twice with n number
             PowersOf2UntilN(4);
             PowersOf2UntilN(6);
 
+            //call method twice with n1 and n2 numbers
             PowersOf2UntilN(4 ,6);
             PowersOf2UntilN(3, 7);
         }
