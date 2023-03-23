0ff44fe4de7956a885957bdba126375fa1e5b3e3
Vitor <vitor.daniel@outlook.pt>
2023-03-23 16:11:11 +0000

    added XML documentation

--- Power2Method/Program.cs
+++ Power2Method/Program.cs
@@ -5,6 +5,9 @@ namespace Power2Method
     class Program
     {
         //method for shift 1 five times
+        /// <summary>
+        /// method for shift 1 five times
+        /// </summary>
         private static void PowersOf2Until5()
         {
             //changed method to use PowersOf2UntilN()
@@ -12,6 +15,10 @@ namespace Power2Method
         }
 
         //method for shift 1 n times
+        /// <summary>
+        /// method for shift 1 n times
+        /// </summary>
+        /// <param name="n">variable for number of times method shifts left</param>
         private static void PowersOf2UntilN(int n)
         {
             for (int i = 1; i <= ( 1 << n ); i = i << 1)
@@ -21,6 +28,10 @@ namespace Power2Method
         }
 
         //main method
+        /// <summary>
+        /// main method
+        /// </summary>
+        /// <param name="args">method accepts strings as type</param>
         static void Main(string[] args)
         {
             PowersOf2Until5();
